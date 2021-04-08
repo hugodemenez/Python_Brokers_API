c9ccbb2766bd666fde42ceade13cb02d6dbbe167
Hugo Demenez <hdemenez@hotmail.fr>
2021-04-08 17:29:16 +0200

    releasing package

--- setup.py
+++ setup.py
@@ -1,24 +1,29 @@
 from distutils.core import setup
 setup(
-  name = 'Python_Brokers_API',         # How you named your package folder (MyLib)
-  packages = ['Python_Brokers_API'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'A package to make requests to brokers like binance,kraken',   # Give a short description about your library
-  author = 'Hugo Demenez',                   # Type in your name
-  author_email = 'hdemenez@hotmail.fr',      # Type in your E-Mail
-  url = 'https://github.com/hugodemenez/Python_Brokers_API',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['Python', 'Brokers', 'API'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+  name = 'Python_Brokers_API',         
+  packages = ['Python_Brokers_API'],   
+  version = '0.2',      
+  license='MIT',        
+  description = 'A package to make requests to brokers like binance,kraken',   
+  author = 'Hugo Demenez',                  
+  author_email = 'hdemenez@hotmail.fr',     
+  url = 'https://github.com/hugodemenez/Python_Brokers_API',   
+  download_url = 'https://github.com/hugodemenez/Python_Brokers_API/archive/refs/tags/v0.2.tar.gz', 
+  keywords = ['Python', 'Brokers', 'API'],   
+  install_requires=[           
+          'time',
+          'json',
+          'hmac',
+          'hashlib',
+          'requests',
+          'krakenex',
+          'urllib',
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha',     
+    'Intended Audience :: Developers',     
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3.9',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3.9',    
   ],
 )
